2e952ec39b8528d8e4489d5bf30e4cd7fb06f0a9
TheSaltyGaming <38183890+TheSaltyGaming@users.noreply.github.com>
2023-01-29 13:16:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const ip = "saltygaming.no";
+const axios = require("axios");
+
+const ip = "192.168.0.1";
 const ports = [25565, 25566, 25567, 25568, 25569];
 
 async function updateStatus() {
   for (const port of ports) {
-    const response = await fetch(`http://${ip}:${port}`);
-    document.getElementById(`server${port - 25564}`).innerHTML =
-      response.status === 200 ? "Online" : "Offline";
-    document.getElementById(`server${port - 25564}`).className =
-      response.status === 200 ? "online" : "offline";
+    const response = await axios.get(`http://${ip}:${port}`);
+    console.log(`Server${port - 25564}: ${response.status === 200 ? "Online" : "Offline"}`);
   }
 }
 
